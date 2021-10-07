70b7c633e537f7952163214f6a0e412924cf7c81
Paul Hendricks <paul@digitalhurricane.io>
2021-10-06 18:31:48 -0600

    add to templates. change comments

--- templates.go
+++ templates.go
@@ -15,12 +15,17 @@ func (t Template) MissingParams() templateResponse {
 	return newTemplateResponse(t.rw, 400, fmtErr("Missing or invalid params"))
 }
 
+// BadRequest 400 status {"error": "Bad request"}
+func (t Template) BadRequest() templateResponse {
+	return newTemplateResponse(t.rw, 400, fmtErr("Bad request"))
+}
+
 // NotFound 404 status {"error": "Resource not found"}
 func (t Template) NotFound() templateResponse {
 	return newTemplateResponse(t.rw, 404, fmtErr("Resource not found"))
 }
 
-// InternalError Set generic error message {"error": "An error occurred"}
+// InternalError 500 status {"error": "An error occurred"}
 func (t Template) InternalError() templateResponse {
 	return newTemplateResponse(t.rw, 500, fmtErr("An error occurred"))
 }
@@ -30,7 +35,7 @@ func (t Template) DBError() templateResponse {
 	return newTemplateResponse(t.rw, 500, fmtErr("DB Error"))
 }
 
-// JsonParseError 400 status {"error": "JSON badly formatted"}
+// JsonParseError 400 status {"error": "JSON incorrectly formatted"}
 func (t Template) JsonParseError() templateResponse {
 	return newTemplateResponse(t.rw, 500, fmtErr("JSON badly formatted"))
 }
